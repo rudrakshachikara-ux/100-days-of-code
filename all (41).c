98bb87ad40055c09d1c8e45cce3af4ecdb52c9b1
rudrakshachikara-ux <rudrakshachikara@gmail.com>
2025-09-13 12:01:45 +0530

    Add files via upload

---
+++ all (41).c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+    int n, i;
+    printf("Enter the value of n:");
+    scanf("%d", &n);
+    for (i= 1; i <= n; i++) {
+    printf("%d", i);
+    }
+    printf("\n");
+    return 0;
+}
+    
+
+
